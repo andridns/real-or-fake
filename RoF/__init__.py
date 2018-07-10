56f46894d2afa73f381506f82d55820fcbaedae2
Andri Danusasmita <andridsasmita@gmail.com>
2018-07-10 10:41:35 +0900

    Add templates, node modules and static files

---
+++ RoF/__init__.py
@@ -0,0 +1,45 @@
+import os
+import csv
+import smtplib
+import random
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from flask import Flask, redirect, render_template, request
+from flask_restful import Resource, Api
+from flask.json import jsonify
+import sqlite3
+
+app = Flask(__name__)
+api = Api(app)
+
+# PATH = '/var/www/RoF/RoF/' # server
+PATH = 'C:/Users/adsasmita/Desktop/RoF/RoF/' # local
+cols = ['index','author','comment','line','truth']
+DB_PATH = PATH+"db/"
+    
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+class Popular_Magazine(Resource):
+    def get(self, idx):
+        conn = sqlite3.connect(DB_PATH+'popMag.db')
+        qstr = f"select * from popMag WHERE id={idx}"
+        query = conn.execute(qstr).fetchall()[0]
+        result = dict(zip(cols,list(query)))
+        return jsonify(result)
+
+class Kompas(Resource):
+    def get(self, idx):
+        conn = sqlite3.connect(DB_PATH+'kompas.db')
+        qstr = f"select * from kompas WHERE id={idx}"
+        query = conn.execute(qstr).fetchall()[0]
+        result = dict(zip(cols,list(query)))
+        return jsonify(result)
+
+api.add_resource(Popular_Magazine, '/labels/pop_mag/<idx>') # Route_3
+api.add_resource(Kompas, '/labels/kompas/<idx>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
