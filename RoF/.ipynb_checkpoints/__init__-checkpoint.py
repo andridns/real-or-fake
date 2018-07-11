d053270de5acb049de6ec11ba257ef2f46f6310c
Andri Danusasmita <andridsasmita@gmail.com>
2018-07-11 19:54:36 +0900

    Add records database and JSON-SQL integration

--- RoF/.ipynb_checkpoints/__init__-checkpoint.py
+++ RoF/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,45 +1,33 @@
 import os
+import csv
+import smtplib
+import random
+import numpy as np
 import pandas as pd
+from pathlib import Path
 from flask import Flask, redirect, render_template, request
 from flask_restful import Resource, Api
 from flask.json import jsonify
-from datetime import datetime, timedelta
 import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
-PATH = '/var/www/RoF/RoF/' # server
-# PATH = 'C:/Users/adsasmita/Desktop/RoF/RoF/' # local
+# PATH = '/var/www/RoF/RoF/' # server
+PATH = 'C:/Users/adsasmita/Desktop/RoF/RoF/' # local
 cols = ['index','author','comment','line','truth']
 DB_PATH = PATH+"db/"
-
-def timenow(): return datetime.utcnow()
-def dt2id(dt): return int((dt-datetime(2018,7,11)).total_seconds()*10000)
-def id2dt(dtid): return datetime(2018,7,11)+timedelta(seconds=dtid/10000)
-
+    
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/append_quiz', methods=['POST'])
-def append_quiz():
-    dt = timenow()
-    dtid = dt2id(dt)
-    conn = sqlite3.connect(DB_PATH+'Records.db')
-    d = conn.cursor()
-    rows = request.json['data']
-    for row in rows:
-        user_name, dataset, line_index, line, correct = \
-            row['user_name'], row['dataset'], row['index'], row['line'], row['correct']
-        d.execute('INSERT INTO Records (DTID, Datetime, UserName, Dataset, LineIndex, Line, Correct)'
-                  'VALUES (?,?,?,?,?,?,?)', (dtid, dt, user_name, dataset, line_index, line, correct))
-    conn.commit()
-    conn.close()
-    resp = {"append_status":True}
-    print(resp)
-    return jsonify(resp)
-    
+@app.route('/api/add_message/<uuid>', methods=['POST'])
+def add_message(uuid):
+    content = request.json
+    print(content['mytext'])
+    return jsonify({"uuid":uuid})
+
 class Popular_Magazine(Resource):
     def get(self, idx):
         conn = sqlite3.connect(DB_PATH+'popMag.db')
